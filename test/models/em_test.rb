c2f072a4bd5bdcf0309793fbab2df8fe98441856
Magmagan <alexlalves@live.com>
2020-11-09 17:55:41 -0300

    add sanity test cases for join table use for both models

--- test/models/em_test.rb
+++ test/models/em_test.rb
@@ -5,4 +5,15 @@ class EmTest < ActiveSupport::TestCase
     assert Em.create(mane: "first em")
     assert Em.where(mane: "first em").count == 1
   end
+
+  test "join table working as expected" do
+    en = En.create(name: "enn")
+
+    em = Em.new
+    em.mane = "mm"
+    em.ens << en
+
+    assert em.save
+    assert_includes Em.find_by_mane("mm").ens, en
+  end
 end
--- test/models/en_test.rb
+++ test/models/en_test.rb
@@ -5,4 +5,15 @@ class EnTest < ActiveSupport::TestCase
     assert En.create(name: "first en")
     assert En.where(name: "first en").count == 1
   end
+
+  test "join table working as expected" do
+    em = Em.create(mane: "emm")
+
+    en = En.new
+    en.name = "nn"
+    en.ems << em
+
+    assert en.save
+    assert_includes En.find_by_name("nn").ems, em
+  end
 end
